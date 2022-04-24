50d0800f8a7502f6bef63e191ec7d8f2513833b6
Arcadii Semenov <arcadii.sem@gmail.com>
2022-04-24 17:22:00 +0300

    Update Main.java

--- GeometryDash_gameswithgabe/src/com/main/Main.java
+++ GeometryDash_gameswithgabe/src/com/main/Main.java
@@ -4,7 +4,6 @@ import com.jade.Window;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
         Window window = Window.getWindow();
         window.init();
         Thread mainThread = new Thread(window);
